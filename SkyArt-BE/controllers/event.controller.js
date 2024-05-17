a1d7752c952e7721c9ad5e70416b44d1d8dac391
Helmi Bacha <helmibacha5@gmail.com>
2024-05-17 19:02:09 +0100

    End Date Event Imp

--- SkyArt-BE/controllers/event.controller.js
+++ SkyArt-BE/controllers/event.controller.js
@@ -1,8 +1,6 @@
 import Event from '../models/Event.js';
-import express from 'express';
 import mongoose from 'mongoose';
 
-const router = express.Router();
 
 export const getEvents = async (req, res, next) => {
     
@@ -27,9 +25,9 @@ export const getEvent = async (req, res) => {
 }
 
 export const createEvent = async (req, res) => {
-    const { title, fromDate, nbrAttendees, isExpired, organizedBy, link, isPaid} = req.body;
+    const { title, fromDate, endDate, nbrAttendees, isExpired, organizedBy, link, isPaid} = req.body;
 
-    const newEvent = new Event({ title, fromDate, nbrAttendees, isExpired, organizedBy, link, isPaid })
+    const newEvent = new Event({ title, fromDate, endDate, nbrAttendees, isExpired, organizedBy, link, isPaid })
 
     try {
         await newEvent.save();
@@ -44,11 +42,11 @@ export const updateEvent = async (req, res) => {
     const { id } = req.params;
     const { title, fromDate, endDate, nbrAttendees, isExpired, organizedBy, link, isPaid} = req.body;
     
-    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No user with id: ,${id}`);
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No event with id: ,${id}`);
 
     const updatedEvent = { title, fromDate, endDate, nbrAttendees, isExpired, organizedBy, link, isPaid, _id: id };
 
-    await User.findByIdAndUpdate(id, updatedEvent, { new: true });
+    await Event.findByIdAndUpdate(id, updatedEvent, { new: true });
 
     res.json(updatedEvent);
 };
