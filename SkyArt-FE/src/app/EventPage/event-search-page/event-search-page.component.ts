0e95233b486d0e181ed3d7f28a0cf5a2e9bdb46e
Helmi Bacha <helmibacha5@gmail.com>
2024-07-05 19:51:36 +0100

    commit events FE

--- SkyArt-FE/src/app/EventPage/event-search-page/event-search-page.component.ts
+++ SkyArt-FE/src/app/EventPage/event-search-page/event-search-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {EventService} from "../../services/event.service";
+import {HttpClient} from "@angular/common/http";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-search-page',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventSearchPageComponent implements OnInit {
 
-  constructor() { }
+  /*constructor(private http:HttpClient) {
+    this.http.get('http://localhost:4040/event/events/').subscribe(data=>console.log(data))
+  }
+  ngOnInit(): void {
+    
+  }*/
+  public events:any;
+  public errorMsg:any;
+  constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
+    this.eventService.getEvents().subscribe(
+      (data:any = [])=>{
+        this.events = data;
+        console.log(this.events);
+      },error => {
+        console.log(error);
+        this.errorMsg  = error;
+      }
+    );
   }
 
 }
+  /*dataArray:any=[]
+ 
+  dataEvent={
+    title:'',
+    fromDate:'',
+    endDate:'',
+    organizedBy:'',
+    nbrAttendees:0,
+    link:'',
+    isPaid:'',
+  }
+
+  messageSuccess=''
+  constructor(private ds:EventService,private route:Router) {
+   
+    this.ds.getEvents().subscribe(data=>{
+      console.log(data)
+      this.dataArray=Object.values(data)
+    })
+    
+   }
+
+  ngOnInit(): void {
+  }*/
+
+
