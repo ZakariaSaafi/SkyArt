46c9bc3a9d797d3986e15ed6f498d15b8d4d0b7d
khaoulachaieb <Khaoula.CHAIEB@esprit.tn>
2024-07-05 20:44:01 +0100

    fixes

--- SkyArt-FE/src/app/HomePage/header/header.component.ts
+++ SkyArt-FE/src/app/HomePage/header/header.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { jwtDecode } from 'jwt-decode';
-import { User } from '../../../models/user.model';
-import {FeedbackService} from "../../../services/feedback/feedback.service";
-
 
 @Component({
   selector: 'app-header',
@@ -10,36 +6,10 @@ import {FeedbackService} from "../../../services/feedback/feedback.service";
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  userId: string = '';
-  user!: User;
 
-  constructor(private feedbackService: FeedbackService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.userId = this.getUserIdFromToken();
-    if (this.userId) {
-      this.loadUser();
-    }
   }
 
-  getUserIdFromToken(): string {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken: any = jwtDecode(token);
-      return decodedToken.userId;
-    }
-    return '';
-  }
-
-  loadUser(): void {
-    this.feedbackService.getUserById(this.userId).subscribe(
-      (user: any) => {
-        this.user = user.user;
-        console.log('User data:', user);
-      },
-      (error) => {
-        console.error('Error fetching user data:', error);
-      }
-    );
-  }
 }
